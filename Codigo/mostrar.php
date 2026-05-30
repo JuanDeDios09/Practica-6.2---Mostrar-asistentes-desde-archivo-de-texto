f9a1831a2a9ef37014cb5f852711b0b08b16908e
JuanDeDios09 <252310070@itslerdo.edu.mx>
2026-05-30 16:15:28 -0600

    Se agrego mostrar.php

---
+++ Codigo/mostrar.php
@@ -0,0 +1,31 @@
+<?php
+// nombre del archivo que contene los asistentes
+// Este script lee un archivo de textto y muestra los nombres de los asistentes en una lista eneumerada
+$archivo = "asistentes.txt";
+
+try {
+    // Verifica si el archivo existe 
+    if (!file_exists($archivo)) {
+        throw new Exception("El archivo no existe.");
+    }
+
+    //Abrir el archivo para lectura 
+    $fp = fopen($archivo, "r");
+
+    // Si no pudo abrir el archivo, lanzamos una excepcion
+    $contador = 1;
+    while (!feof($fp)) {
+        // Leemos una linea del archivo
+        $linea = fgets($fp);
+
+        //htmlspecialchars() para evitar problemas de seguridad con HTML
+        echo $contador . ".". htmlspecialchars(trim($linea)) . "<br>";
+        $contador++;
+        
+    }
+
+    fclose($fp);
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
